baa4dd4d542db1682298fa86196bc5105f18b648
sngtmth <sngtmth@gmail.com>
2021-09-29 23:21:09 +0530

    Added Reminder and Quotient Program

---
+++ RemainderQuotient.java
@@ -0,0 +1,16 @@
+package com.day5programs;
+
+public class RemainderQuotient {
+
+	public static void main(String[] args) {
+		int dividend = 78, divisor = 9;
+
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+
+		System.out.println("The Quotient is = " + quotient);
+		System.out.println("The Remainder is = " + remainder);
+
+	}
+
+}
